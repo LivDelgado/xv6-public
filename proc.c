02e90481ddb94ef3d19e38c30ec14f5834d33671
Gabriel Chaves <gabrielbum@gmail.com>
2022-02-02 22:24:40 -0300

    alterado scheduler para implementar o priority based round robin scheduling walkthrough

--- proc.c
+++ proc.c
@@ -333,25 +333,32 @@ int wait(void)
 //      via swtch back to the scheduler.
 void scheduler(void)
 {
-  struct proc *p;
+  struct proc *p, *p1;
   struct cpu *c = mycpu();
   c->proc = 0;
-
-  for (;;)
-  {
+  
+  for(;;){
     // Enable interrupts on this processor.
     sti();
-
+    struct proc *highP;
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
-    for (p = ptable.proc; p < &ptable.proc[NPROC]; p++)
-    {
-      if (p->state != RUNNABLE)
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->state != RUNNABLE)
         continue;
 
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
+      highP = p;
+      //choose one with highest priority
+      for(p1 = ptable.proc; p1 < &ptable.proc[NPROC]; p1++){
+	if(p1->state != RUNNABLE)
+	  continue;
+	if(highP->priority > p1->priority)   //larger value, lower priority
+	  highP = p1;
+      }
+      p = highP;
       c->proc = p;
       switchuvm(p);
       p->state = RUNNING;
@@ -364,6 +371,7 @@ void scheduler(void)
       c->proc = 0;
     }
     release(&ptable.lock);
+
   }
 }
 
