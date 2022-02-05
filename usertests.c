4646828178b5a9fc7b11903db83fcf8ff66677a2
Lívia Delgado <livia.delgado.ac@gmail.com>
2022-02-05 16:22:02 -0300

    atualiza formato dos arquivos de teste

--- usertests.c
+++ usertests.c
@@ -3,36 +3,42 @@
 #include "user.h"
 #include "fcntl.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
   int pid;
   int k, n;
   int x, z;
 
-  if(argc < 2)
-	n = 1; //Default
+  if (argc < 2)
+    n = 1; //Default
   else
-	n = atoi(argv[1]);
-  if (n < 0 ||n > 20)
-	n = 2;
+    n = atoi(argv[1]);
+  if (n < 0 || n > 20)
+    n = 2;
   x = 0;
   pid = 0;
 
-  for ( k = 0; k < n; k++ ) {
-    pid = fork ();
-      printProcesses();
-    if ( pid < 0 ) {
+  for (k = 0; k < n; k++)
+  {
+    pid = fork();
+    printProcesses();
+    if (pid < 0)
+    {
       printf(1, "%d failed in fork!\n", getpid());
-    } else if (pid > 0) {
+    }
+    else if (pid > 0)
+    {
       // parent
       wait();
-      }
-      else{
-	//printf(1,"Child %d created\n",getpid());
-	for(z = 0; z < 400000000; z+=1)
-	    x = x + 3.14*89.64; //Useless calculation to consume CPU Time
-	break;
-      }
+    }
+    else
+    {
+      //printf(1,"Child %d created\n",getpid());
+      for (z = 0; z < 400000000; z += 1)
+        x = x + 3.14 * 89.64; //Useless calculation to consume CPU Time
+      break;
+    }
   }
-  
-  exit(); 
+
+  exit();
 }
